7419430e7a8fd7a2d73ff35436dfafec255ce40c
Jang Jae hyeok <121391545+thisjheok@users.noreply.github.com>
2024-08-06 21:30:26 +0900

    Update Login.jsx

--- src/pages/dongjae/Login.jsx
+++ src/pages/dongjae/Login.jsx
@@ -1,113 +1,108 @@
 import React from "react";
 import styled from "styled-components";
-import Layout from "../../pages/dongjae/Layout";
 import { useNavigate } from "react-router-dom";
 
+const GlobalStyle = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body, html {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+`;
+
 const Background = styled.div`
-    width:376px;
-    height:812px;
-    background-color:#D94844;
-`
+  width: 100vw;
+  height: 100vh;
+  background-color: #D94844;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
 
-const Text1 = styled.img`
-position: absolute;
-width: 319px;
-height: 142px;
-left: calc(50% - 319px/2 - 0.5px);
-top: 241px;
+const ContentWrapper = styled.div`
+  width: 100%;
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-font-family: 'Open Sans';
-font-style: normal;
-font-weight: 700;
-font-size: 64px;
-line-height: 87px;
+const LogoText = styled.div`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 700;
+  font-size: 48px;
+  color: #FFFFFF;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
 
-color: #FFFFFF;
-`
-const Text2 = styled.img`
-position: absolute;
-width: 352px;
-height: 58px;
-left: calc(50% - 352px/2);
-top: 406px;
+const LogoIcon = styled.img`
+  width: 48px;
+  height: 48px;
+  margin-left: 10px;
+`;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 29px;
-text-align: center;
+const SubText = styled.div`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  text-align: center;
+  color: #FFFFFF;
+  margin-bottom: 40px;
+`;
 
-color: #FFFFFF;
-`
-
-const WhiteLogo = styled.img`
-    /* image 4 */
-
-position: absolute;
-width: 80px;
-height: 80px;
-left: 210px;
-top: 241px;
-`
-const Sellerbutton = styled.button`
-
-position: absolute;
-width: 336px;
-height: 52px;
-top: 624px;
-border-radius:15px;
-left:22px;
-border:none;
-display:flex;
-align-items:center;
-justify-content:center;
-cursor:pointer;
-`
-const Customerbutton = styled.button`
-position: absolute;
-width: 336px;
-height: 52px;
-top: 684px;
-left:22px;
-border-radius:15px;
-border:none;
-display:flex;
-align-items:center;
-justify-content:center;
-cursor:pointer;
-`
-const ButtonText = styled.p`
-
-/* TItle/20px/Bold */
-font-family: 'Open Sans';
-font-style: normal;
-font-weight: 700;
-font-size: 20px;
-line-height: 27px;
-
-/* main */
-color: #D94844;
-`
+const Button = styled.button`
+  width: 100%;
+  max-width: 336px;
+  height: 52px;
+  background-color: #FFFFFF;
+  border-radius: 15px;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin-bottom: 16px;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  color: #D94844;
+`;
 
 const Login = () => {
-    const navigate = useNavigate();
-    return(
-        <>
-            <Background>
-                <Text1 src="assets/logo_text.svg" />
-                <Text2 src="assets/main_text.svg"/>
-                <WhiteLogo src="assets/white_logo.svg"/>
-                <Sellerbutton onClick={() => navigate("/sellerhome")}>
-                    <ButtonText>판매자로 시작하기</ButtonText>
-                </Sellerbutton>
-                <Customerbutton onClick={() => navigate("/customer_start")}>
-                    <ButtonText>구매자로 시작하기</ButtonText>
-                </Customerbutton>
-
-            </Background>
-        </>
-    );
+  const navigate = useNavigate();
+  return (
+    <>
+      <GlobalStyle />
+      <Background>
+        <ContentWrapper>
+          <LogoText>
+            떠리원
+            <LogoIcon src="assets/white_logo.svg" alt="Logo" />
+          </LogoText>
+          <SubText>
+            우리 동네 떨이 제품을<br />
+            마감 전에 쉽게 판매 / 구매해보세요
+          </SubText>
+          <Button onClick={() => navigate("/sellerhome")}>
+            판매자로 시작하기
+          </Button>
+          <Button onClick={() => navigate("/customer_start")}>
+            구매자로 시작하기
+          </Button>
+        </ContentWrapper>
+      </Background>
+    </>
+  );
 };
 
 export default Login;
